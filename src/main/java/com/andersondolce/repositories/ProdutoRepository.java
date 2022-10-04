fd938294064caa6aa9985b3cf3502722a546e7ae
Anderson Dolce <dolce436@gmail.com>
2022-10-04 15:06:57 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/andersondolce/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.andersondolce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andersondolce.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{	
+	
+}
