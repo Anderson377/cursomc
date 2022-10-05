1f17cd6f33bb9b0da5db9642efb6d7162d97bc31
Anderson Dolce <dolce436@gmail.com>
2022-10-05 13:49:37 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/andersondolce/resources/CategoriaResource.java
+++ src/main/java/com/andersondolce/resources/CategoriaResource.java
@@ -1,7 +1,6 @@
 package com.andersondolce.resources;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +22,7 @@ public class CategoriaResource {
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
-		
+		Categoria obj = service.buscar(id);		
 		return ResponseEntity.ok().body(obj); 		
 		
 	}
