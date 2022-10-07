e498ddcc8d2bca6279af5fec24de5d64c5fc913c
Anderson Dolce <dolce436@gmail.com>
2022-10-07 12:03:00 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/andersondolce/domain/Estado.java
+++ src/main/java/com/andersondolce/domain/Estado.java
@@ -2,7 +2,9 @@ package com.andersondolce.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -25,6 +27,7 @@ public class Estado implements Serializable{
 	@JsonBackReference
 	@OneToMany(mappedBy = "estado")
 	private List<Cidade> cidades = new ArrayList<>();
+	
 	
 	public Estado() {
 		
